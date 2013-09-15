d5f81cb65044dca1cc942aef6dfc328e638e6b61
ForNeVeR <neverthness@gmail.com>
2013-09-15 22:37:36 +0700

    Use 2.9.0 libraries. Thanks, rexim.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[log4j/log4j "1.2.17"]
-                 [org.clojars.rexim/lwjgl-platform-natives "2.8.4"]
+                 [org.clojars.rexim/lwjgl-platform-natives "2.9.0"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.lwjgl.lwjgl/lwjgl "2.8.4"]]
+                 [org.lwjgl.lwjgl/lwjgl "2.9.0"]]
   :main discoimage.application)
