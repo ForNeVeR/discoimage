82eeff6b79edb26fe03e52ac9e3a30f4a21b876e
ForNeVeR <neverthness@gmail.com>
2013-09-15 19:36:27 +0700

    All tests passing.

--- src/discoimage/logic.clj
+++ src/discoimage/logic.clj
@@ -1,20 +1,32 @@
 (ns discoimage.logic)
 
+(defn picture-width
+  "Determine X dimension of the picture."
+  [picture]
+  (count (picture 0)))
+
+(defn picture-height
+  "Determine Y dimension of the picture."
+  [picture]
+  (count picture))
+
 (defn picture-has-pixel?
   "Determines whether the picture has selected pixel enabled."
   [picture x y]
-  (=
-    1
-    (-> picture
-    (nth y)
-    (nth x))))
+  (let [y-index (- (picture-height picture) y 1)
+        value (-> picture
+                (nth y-index)
+                (nth x))]
+    (= value 1)))
 
 (defn led-enabled?
   "Checks whether the led should be enabled."
   [picture angle led]
-  (let [x (* led (Math/cos angle))
-        y (* led (Math/sin angle))]
-    (picture-has-pixel? picture x y)))
+  (let [center-x (/ (picture-width picture) 2)
+        center-y (/ (picture-height picture) 2)
+        x (+ center-x (* led (Math/cos angle)))
+        y (+ center-y (* led (Math/sin angle)))]
+    (picture-has-pixel? picture (Math/floor x) (Math/floor y))))
 
 (defn calculate
   "Calculates the LED states."
--- test/discoimage/logic_test.clj
+++ test/discoimage/logic_test.clj
@@ -6,13 +6,13 @@
   (testing "Testing picture-has-pixel function."
     (is (true? (picture-has-pixel? [[1]] 0 0)))
     (is (true? (picture-has-pixel? [[1 0]
-                                   [0 1]] 1 1)))
+                                    [0 1]] 1 0)))
     (is (false? (picture-has-pixel? [[1 0]
-                                    [0 1]] 0 1)))))
+                                     [0 1]] 1 1)))))
 
 (deftest led-enabled-test
   (testing "Testing led-enabled function."
     (let [picture [[1 0]
                    [0 1]]]
-      (is (false? (led-enabled? picture 0.5 (* 1/4 Math/PI))))
-      (is (true? (led-enabled? picture 0.5 (* 3/4 Math/PI)))))))
+      (is (false? (led-enabled? picture (* 1/4 Math/PI) 0.5)))
+      (is (true? (led-enabled? picture (* 3/4 Math/PI) 0.5))))))
